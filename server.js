9e9a842c9e7ffbcea9d14d2517f8ffba15ba68f0
PabloMoreno2000 <pamta2000@gmail.com>
2020-10-13 13:00:09 -0500

    Added base server with DB connection

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+
+const PORT = 5070;
+
+// Connect to the database
+connectDB();
+// Ask the server to accept JSON objects in the body of the POST/GET requests
+app.use(express.json({ extended: false }));
+
+// Make a request to "http://localhost:5070" to see if it's running
+app.get("/", (req, res) => res.send("API Running"));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
