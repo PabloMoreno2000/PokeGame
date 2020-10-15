94df67cabbd586b20e87a7158da450f56a8445a2
PabloMoreno2000 <pamta2000@gmail.com>
2020-10-15 15:30:38 -0500

    Added constraint to card type creation route

--- routes/admin/managetypes.js
+++ routes/admin/managetypes.js
@@ -1,27 +1,29 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const {check, validationResult} = require("express-validator")
+const { check, validationResult } = require("express-validator");
 const axios = require("axios");
 const CardType = require("../../models/CardType");
 const Card = require("../../models/Card");
 
-
-router.post("/addCardType", async(req, res) =>{
+router.post(
+  "/addCardType",
+  [check("name", "Please insert a name for the new type").exists()],
+  async (req, res) => {
     try {
-        const name = req.body.name;
-        let cardType = await CardType.findOne({name});
-        if(cardType){
-            res.status(400).send("Card type already exists");
-        }else{
-            cardType = new CardType({name});
-            await cardType.save();
-            res.json(cardType)
-        }
+      const name = req.body.name;
+      let cardType = await CardType.findOne({ name });
+      if (cardType) {
+        res.status(400).send("Card type already exists");
+      } else {
+        cardType = new CardType({ name });
+        await cardType.save();
+        res.json(cardType);
+      }
     } catch (error) {
-        console.log(error);
-        res.statusMessage(500).send("Server error")
+      console.log(error);
+      res.statusMessage(500).send("Server error");
     }
-
-});
+  }
+);
 
 module.exports = router;
