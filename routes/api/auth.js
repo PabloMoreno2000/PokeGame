c1112321441f4ce1dbed8ee1d31d9c7082a70e88
PabloMoreno2000 <pamta2000@gmail.com>
2020-10-15 15:48:05 -0500

    Added route to authenticate user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -19,3 +19,70 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route  POST api/auth
+// @desct  Authenticate user & get token
+// @access Public/non-authentication/no-token
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Please is required").exists(),
+  ],
+  async (req, res) => {
+    // The check is done with the second parameter of above within []
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    const { email, password } = req.body;
+
+    try {
+      // If there's a user with that email
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // Check if the hash version of a non-encrypted text equals to a hash
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id, // Moongose gets the id of the database
+        },
+      };
+
+      // Generate a JSON Web Token (encrypted payload with signature)
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"), // encryption key
+        { expiresIn: 360000 },
+        // Callback
+        (err, token) => {
+          // If there's an error, throw it
+          if (err) throw err;
+          // Else, set in the json of the response this web token with
+          // the user id that can be used for authentication after sign up
+          res.json({ token });
+        }
+      );
+
+      // res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
