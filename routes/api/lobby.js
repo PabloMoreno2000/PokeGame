dfb03347c770faf04dd2be49e4ee0348dea74d54
PabloMoreno2000 <pamta2000@gmail.com>
2020-11-04 09:12:42 -0600

    Added lobby route to get all of them

--- routes/api/lobby.js
+++ routes/api/lobby.js
@@ -19,61 +19,55 @@ ready1 == true
 ready2 == true
 */
 
-//TODO: 
-
 // @route  GET api/lobby/getState
-// @desct long polling for lobby
-// @access Private
-router.get('/getState/:id', function(req,res){
-    res.json(rooms.get(parseInt(req.params.id)));
+// @desct  long polling for lobby
+// @access Public
+router.get("/getState/:id", function (req, res) {
+  res.json(rooms.get(parseInt(req.params.id)));
 });
 
 // @route  POST api/lobby/create
-// @desct Create a lobby id for joining the game
+// @desct  Create a lobby id for joining the game
 // @access Public
-router.post('/create',function(req,res){
-    const roomInfo = {
-        "roomId": ++nextId,
-        "player1": null,
-        "player2" : null,
-        "ready1" : false,
-        "ready2" : false
-    }
-    rooms.set(nextId,roomInfo);
-    res.json(roomInfo);
+router.post("/create", function (req, res) {
+  const roomInfo = {
+    roomId: nextId++,
+    player1: null,
+    player2: null,
+    ready1: false,
+    ready2: false,
+  };
+  rooms.set(nextId, roomInfo);
+  res.json(roomInfo);
 });
 
 // @route  PUT api/lobby/join/:id
-// @desct
+// @desct  Join existing lobby
 // @access Private
-// Join existing lobby
-router.put("/join/:lobbyId",auth, function(req, res){
-    if(!rooms.has(rooms.get(parseInt(req.params.lobbyId))))
-    {
-        console.log("room doesnt exist");
-        return res.status(404).send("Room not found");
-    } 
-    var room = rooms.get(parseInt(req.params.lobbyId));
-    if(room.player1 != null && room.player2 != null)
-    {
-        return res.status(400).send("Room full");
-    }
-    // Check if there's free space for player 1
-    if(room.player1 == null)
-    {
-        room.player1 = req.user.id;
-    }
-    else
-    {
-        room.player2 = req.user.id;
-    }
-    rooms.set((parseInt(req.params.lobbyId),room));
-    res.json(room);
+router.put("/join/:lobbyId", auth, function (req, res) {
+  if (!rooms.has(rooms.get(parseInt(req.params.lobbyId)))) {
+    console.log("room doesnt exist");
+    return res.status(404).send("Room not found");
+  }
+  var room = rooms.get(parseInt(req.params.lobbyId));
+  if (room.player1 != null && room.player2 != null) {
+    return res.status(400).send("Room full");
+  }
+  // Check if there's free space for player 1
+  if (room.player1 == null) {
+    room.player1 = req.user.id;
+  } else {
+    room.player2 = req.user.id;
+  }
+  rooms.set((parseInt(req.params.lobbyId), room));
+  res.json(room);
 });
 
-//TO-DO
 // @route  GET api/lobby/getAll
 // @desct  Get all lobbies
 // @access Public
+router.get("/getAll", function (req, res) {
+  res.json(rooms);
+});
 
 module.exports = router;
