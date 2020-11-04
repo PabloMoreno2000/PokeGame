27d5fc907067a87342c8a8a1ed66a957d91cf230
PabloMoreno2000 <pamta2000@gmail.com>
2020-11-04 09:58:03 -0600

    Started script to log in

---
+++ client/scripts/Login.js
@@ -0,0 +1,25 @@
+const usernameId = "username-input";
+const passwordId = "password-id";
+const btnLoginId = "login-button";
+const formId = "form-login";
+
+function byId(itemId) {
+  return $(`#${itemId}`);
+}
+
+$(document).ready(async () => {
+  console.log("In ready doc");
+
+  byId(btnLoginId).click(async () => {
+    console.log("In btnLogin function");
+    const username = byId(usernameId).val();
+    const password = byId(passwordId).val();
+    let res = {};
+    try {
+      res = await API.auth.postAuthUser(username, password);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  });
+});
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-
+const cors = require("cors");
 const PORT = 5070;
 
+app.use(cors());
 // Connect to the database
 connectDB();
 // Ask the server to accept JSON objects in the body of the POST/GET requests
@@ -16,6 +17,6 @@ app.use("/admin/managetypes", require("./routes/admin/managetypes"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/game", require("./routes/api/game"));
-app.use("/api/lobby",require("./routes/api/lobby"));
+app.use("/api/lobby", require("./routes/api/lobby"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
