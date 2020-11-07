666ae812720b9774eece5215d7890ca20ac5c525
PabloMoreno2000 <pamta2000@gmail.com>
2020-11-06 23:11:25 -0600

    Added scripts to html files

---
+++ client/scripts/Home.js
@@ -0,0 +1,17 @@
+const createGame = "create-game";
+const joinGame = "join-game";
+
+function byId(itemId) {
+  return $(`#${itemId}`);
+}
+
+$(document).ready(async () => {
+  byId(createGame).click(async () => {
+    // Create lobby
+    const lobby = await API.lobby.createLobby();
+    console.log(lobby);
+    // Put player in lobby
+    await API.lobby.joinLobby(lobby.roomId);
+    // TODO: Go to lobby window
+  });
+});
