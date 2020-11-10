3dcae71323aaf262d0ef6157778a4bd4e7dfd930
PabloMoreno2000 <pamta2000@gmail.com>
2020-11-10 16:48:16 -0600

    Deleted unused comment

--- client/scripts/Game.js
+++ client/scripts/Game.js
@@ -48,30 +48,6 @@ function htmlToNode(htmlText) {
   */
 //name, weight, experience, height, types, img
 let get_pokemon_card = (pokemon, handPos) => {
-  /*
-function displayOverlay(text) {
-    $("<table id='overlay'><tbody><tr><td>" + text + "</td></tr></tbody></table>").css({
-        "position": "fixed",
-        "top": "0px",
-        "left": "0px",
-        "width": "100%",
-        "height": "100%",
-        "background-color": "rgba(0,0,0,.5)",
-        "z-index": "10000",
-        "vertical-align": "middle",
-        "text-align": "center",
-        "color": "#fff",
-        "font-size": "40px",
-        "font-weight": "bold",
-        "cursor": "wait"
-    }).appendTo("body");
-}
-
-function removeOverlay() {
-    $("#overlay").remove();
-}
-
-*/
   const { name, photo, pokemonInfo } = pokemon;
   const { currEnergy, currHp, maxHp } = pokemonInfo;
 
