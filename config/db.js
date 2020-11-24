92a441e26762bbb158c0e306574b74f6216df3b4
PabloMoreno2000 <pamta2000@gmail.com>
2020-11-24 12:59:30 -0600

    Changed uri order

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURI") || process.env.mongoURI;
+const db = process.env.mongoURI || config.get("mongoURI");
 
 // Just state that this is an async function
 const connectDB = async () => {
